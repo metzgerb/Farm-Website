b3f4dba0543c032ab8f0d64ab774793e32756a9b
metzgerb <40550164+metzgerb@users.noreply.github.com>
2018-08-03 17:30:41 -0500

    Finished implementing Sales page
    added images to static and updated sales page which required another module to be installed.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const formatCurrency = require('format-currency');
 
 var app = express();
 var handlebars = require('express-handlebars').create({defaultLayout:'main'});
@@ -27,7 +28,7 @@ app.get('/',function(req,res){
       if(!err && response.statusCode < 400){
          var dataset = JSON.parse(body);
          dataset = dataset.dataset_data.data;
-
+         
          //loop through returned data
          for (var i = 0; i < dataset.length; i++) {
             var data = dataset[i];
@@ -53,21 +54,56 @@ app.get('/',function(req,res){
 });
 
 //render GET for Sales
-app.get('/sales',function(req,res){
-  var context = {};
-  context.dataList = "";
-  res.render('sales', context);
+app.get('/sales',function(req,res,next){
+   var context = {};
+   //check if search term found and build where clause
+   if(req.query.search) {
+      var search = '%'+req.query.search+'%';
+      mysql.pool.query('SELECT * FROM inventory WHERE CONCAT(' + 
+      'IFNULL(id,""), "", IFNULL(descript,""), "",IFNULL(price,""),' +
+      'IFNULL(cond,""), "", IFNULL(stock,"")) LIKE ?', [search],
+      function(err, rows, fields){
+         if(err){
+            next(err);
+            return;
+         }
+         context.results = rows;
+         
+         //reformat price into currency
+         let opts = { format: '%s%v', symbol: '$' }
+         for (var i = 0; i<context.results.length; i++) {
+            context.results[i].price = formatCurrency(context.results[i].price, opts);
+         }
+         
+         res.render('sales', context);
+      });
+   //Select all when no search term found
+   } else {
+      mysql.pool.query('SELECT * FROM inventory', function(err, rows, fields){
+         if(err){
+            next(err);
+            return;
+         }
+         context.results = rows;
+         
+         //reformat price into currency
+         let opts = { format: '%s%v', symbol: '$' }
+         for (var i = 0; i<context.results.length; i++) {
+            context.results[i].price = formatCurrency(context.results[i].price, opts);
+         }
+         
+         res.render('sales', context);
+      });
+   }
 });
 
 //render GET for Service
 app.get('/service',function(req,res){
-
   res.render('service');
 });
 
 //render GET for About
 app.get('/about',function(req,res){
-
   res.render('about');
 });
 
@@ -100,7 +136,7 @@ app.get('/insert',function(req,res,next){
   "('KM126GX','Kubota M126GX Tractor', 60000.00,'Used',1)," +
   "('JDTire','John Deere Tractor Tires', 101.00,'New',5)," +
   "('CIHOil','Case IH 10W-40 Oil - 1 Gal', 17.00,'New',23)," +
-  "('THB60','Tomahawk 60in. Smooth Bucket', 365.00,'new',2)," +
+  "('THB60','Tomahawk 60in. Smooth Bucket', 365.00,'New',2)," +
   "('WWRC15P','Smucker WWRC15-P - 15ft. Weed Wiper 3 Point Hitch Mount', 2579.99,'Used',1)," +
   "('HIDKit','HID Light Kit', 184.00,'New',4)," +
   "('T4DLT','Traveller 4DLT Heavy-Duty Battery', 179.99,'New',3)," +
